ec1e07b1f6916f118e0e90c5306fd31e6bfa30b9
David Saunders <david@monkee-boy.com>
2017-08-30 08:51:52 -0500

    Use connect-store module to store express sessions in mongo

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session); // loading and calling module -- lets connect middleware access the express sessions
 const app = express();
+
+// connect to mongodb
+mongoose.connect("mongodb://localhost:27017/bookworm");
+const db = mongoose.connection;
+// mongo error handler
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // use session for tracking logins
 app.use(session({
   secret: 'treehouse loves you', // required - string used to sign the session id cookie
   resave: true, // forces session to be saved in the session store whether or not anything was changed
-  saveUninitialized: false // forces an uninitialized session to be saved in the session store
+  saveUninitialized: false, // forces an uninitialized session to be saved in the session store,
+  store: new MongoStore({
+    mongooseConnection: db // pass mongo connection
+  })
 }));
 
 // make user ID available in templates
@@ -16,12 +26,6 @@ app.use(function(req, res, next) {
   res.locals.currentUser = req.session.userId; // If user id is stored in session, currentUser will equal that number, otherwise, currentUser will be undefined
   next();
 });
-
-// connect to mongodb
-mongoose.connect("mongodb://localhost:27017/bookworm");
-const db = mongoose.connection;
-// mongo error handler
-db.on('error', console.error.bind(console, 'connection error:'));
 
 // parse incoming requests
 app.use(bodyParser.json());
